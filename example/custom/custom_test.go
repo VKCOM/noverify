6226d7abba323928278b7f24419584f00a14fab9
Yuriy Nasretdinov <y.nasretdinov@corp.vk.com>
2019-03-06 01:10:06 +0300

    Add example of adding a custom check that requires to compare strings using ===

---
+++ example/custom/custom_test.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/VKCOM/noverify/src/linter"
+	"github.com/VKCOM/noverify/src/meta"
+	"github.com/z7zmey/php-parser/node"
+)
+
+func init() {
+	linter.RegisterBlockChecker(func(ctx linter.BlockContext) linter.BlockChecker { return &block{ctx: ctx} })
+	go linter.MemoryLimiterThread()
+}
+
+func testParse(t *testing.T, filename string, contents string) (rootNode node.Node, w *linter.RootWalker) {
+	var err error
+	rootNode, w, err = linter.ParseContents(filename, []byte(contents), "UTF-8", nil)
+	if err != nil {
+		t.Errorf("Could not parse %s: %s", filename, err.Error())
+		t.Fail()
+	}
+
+	if !meta.IsIndexingComplete() {
+		w.UpdateMetaInfo()
+	}
+
+	return rootNode, w
+}
+
+func singleFileReports(t *testing.T, contents string) []*linter.Report {
+	meta.ResetInfo()
+
+	testParse(t, `first.php`, contents)
+	meta.SetIndexingComplete(true)
+	_, w := testParse(t, `first.php`, contents)
+
+	return w.GetReports()
+}
+
+func TestAssignmentAsExpression(t *testing.T) {
+	reports := singleFileReports(t, `<?php
+	// phpdoc annotations are not required for NoVerify in simple cases
+	function something() {
+		$a = "test";
+		return $a;
+	}
+	function in_array() {}
+
+	function test() {
+		$b = ["1", "2", "3"];
+
+		if (in_array(something(), $b)) {
+			echo "third arg true";
+		}
+
+		if (something() == $b[1]) {
+			echo "must be ===";
+		}
+	}
+	`)
+
+	for _, r := range reports {
+		log.Printf("%s", r)
+	}
+
+	if len(reports) != 2 {
+		t.Errorf("Unexpected number of reports: expected 2, got %d", len(reports))
+		if len(reports) < 2 {
+			t.FailNow()
+		}
+	}
+
+	text := reports[0].String()
+
+	if !strings.Contains(text, "3rd argument of in_array must be true when comparing strings (strict comparison)") {
+		t.Errorf("Wrong report text: expected '3rd argument of in_array must be true', got '%s'", text)
+	}
+
+	text = reports[1].String()
+
+	if !strings.Contains(text, "Strings must be compared using '===' operator") {
+		t.Errorf("Wrong report text: expected 'Strings must be compared using '===' operator', got '%s'", text)
+	}
+}
---
+++ example/custom/main.go
@@ -0,0 +1,85 @@
+package main
+
+// This is an example of adding of custom rules
+
+import (
+	"log"
+
+	"github.com/VKCOM/noverify/src/cmd"
+	"github.com/VKCOM/noverify/src/linter"
+	"github.com/VKCOM/noverify/src/meta"
+	"github.com/VKCOM/noverify/src/solver"
+	"github.com/z7zmey/php-parser/node"
+	"github.com/z7zmey/php-parser/node/expr"
+	"github.com/z7zmey/php-parser/node/expr/binary"
+	"github.com/z7zmey/php-parser/node/name"
+	"github.com/z7zmey/php-parser/node/scalar"
+	"github.com/z7zmey/php-parser/walker"
+)
+
+func main() {
+	log.SetFlags(log.Flags() | log.Lmicroseconds)
+	linter.RegisterBlockChecker(func(ctx linter.BlockContext) linter.BlockChecker { return &block{ctx: ctx} })
+	cmd.Main()
+}
+
+type block struct {
+	ctx linter.BlockContext
+}
+
+func isString(ctx linter.BlockContext, n node.Node) bool {
+	_, ok := n.(*scalar.String)
+	if ok {
+		return true
+	}
+
+	return solver.ExprType(ctx.Scope(), ctx.ClassParseState(), n).IsString()
+}
+
+func (b *block) BeforeEnterNode(w walker.Walkable) {
+	switch n := w.(type) {
+	case *expr.FunctionCall:
+		b.handleFunctionCall(n)
+	case *binary.Equal:
+		if isString(b.ctx, n.Left) || isString(b.ctx, n.Right) {
+			b.ctx.Report(n, linter.LevelWarning, "Strings must be compared using '===' operator")
+		}
+	case *binary.NotEqual:
+		if isString(b.ctx, n.Left) || isString(b.ctx, n.Right) {
+			b.ctx.Report(n, linter.LevelWarning, "Strings must be compared using '!==' operator")
+		}
+	}
+}
+
+func (b *block) handleFunctionCall(e *expr.FunctionCall) {
+	nm, ok := e.Function.(*name.Name)
+	if !ok {
+		return
+	}
+
+	if meta.NameEquals(nm, `in_array`) {
+		b.handleInArrayCall(e)
+		return
+	}
+}
+
+func (b *block) handleInArrayCall(e *expr.FunctionCall) {
+	if len(e.Arguments) != 2 {
+		return
+	}
+
+	arg, ok := e.Arguments[0].(*node.Argument)
+	if !ok {
+		return
+	}
+
+	if !isString(b.ctx, arg.Expr) {
+		return
+	}
+
+	b.ctx.Report(e, linter.LevelWarning, "3rd argument of in_array must be true when comparing strings (strict comparison)")
+}
+
+func (b *block) AfterEnterNode(w walker.Walkable)  {}
+func (b *block) BeforeLeaveNode(w walker.Walkable) {}
+func (b *block) AfterLeaveNode(w walker.Walkable)  {}
