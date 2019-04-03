2bd6b90213c162df20dc30947419355ad3a00c72
Iskander Sharipov <quasilyte@gmail.com>
2019-04-03 16:04:25 +0300

    linter: fix typo in check documentation
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- src/linter/report.go
+++ src/linter/report.go
@@ -102,7 +102,7 @@ func init() {
 		{
 			Name:    "unused",
 			Default: true,
-			Comment: `Report potentially unused variabled.`,
+			Comment: `Report potentially unused variables.`,
 		},
 
 		{
