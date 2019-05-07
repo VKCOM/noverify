4bbf4694e21e8ead022600dce9d08bf871b13c52
Iskander Sharipov <quasilyte@gmail.com>
2019-05-07 10:34:43 +0300

    add test that reproduces Phpstorm "static" issue
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- src/linttest/oop_test.go
+++ src/linttest/oop_test.go
@@ -6,6 +6,50 @@ import (
 	"github.com/VKCOM/noverify/src/linttest"
 )
 
+func TestLateStaticBindingForeach(t *testing.T) {
+	// This test comes from https://youtrack.jetbrains.com/issue/WI-28728.
+	// Phpstorm currently reports `hello` call in `$item->getC()->hello()`
+	// as undefined. NoVerify manages to resolve it properly.
+
+	linttest.SimpleNegativeTest(t, `<?php
+class A
+{
+    /**
+     * @return static[]
+     */
+    public static function create()
+    {
+        return [new static()];
+    }
+}
+
+class B extends A
+{
+    /**
+     * @return C
+     */
+    public function getC()
+    {
+        return new C();
+    }
+}
+
+class C
+{
+    /** Hello does nothing */
+    public function hello()
+    {
+        return 'Hello world!';
+    }
+}
+
+$b = new B();
+foreach ($b->create() as $item) {
+    $item->getC()->hello();
+}
+`)
+}
+
 func TestLateStaticBinding(t *testing.T) {
 	linttest.SimpleNegativeTest(t, `<?php
 class Base {
