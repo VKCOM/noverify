ae7cc70b75a43b5f6fc7d1e650d42d4f9412e61c
R. Tony Goold <tony@goold.net>
2019-06-05 16:45:17 -0400

    fixup! Make compatible with latest php-parser

--- src/linttest/exprtype_test.go
+++ src/linttest/exprtype_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/VKCOM/noverify/src/linttest"
 	"github.com/VKCOM/noverify/src/meta"
 	"github.com/VKCOM/noverify/src/solver"
+	"github.com/z7zmey/php-parser/node"
 	"github.com/z7zmey/php-parser/node/expr/assign"
 	"github.com/z7zmey/php-parser/node/stmt"
 	"github.com/z7zmey/php-parser/walker"
@@ -607,6 +608,8 @@ type globalsWalker struct {
 
 func (gw *globalsWalker) EnterNode(w walker.Walkable) bool {
 	switch n := w.(type) {
+	case *node.Root:
+		return true
 	case *stmt.StmtList:
 		return true
 	case *stmt.Expression:
