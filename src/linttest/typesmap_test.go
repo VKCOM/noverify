596d9899240a9bd6f73f693bf4c566ff7ce20c74
Alex Svetkin <whisk@users.noreply.github.com>
2019-11-22 15:31:49 +0300

    fixed plus (+) operator type inference for arrays (#248)
    Fixes #246

---
+++ src/linttest/typesmap_test.go
@@ -0,0 +1,51 @@
+package linttest
+
+import (
+	"testing"
+
+	"github.com/VKCOM/noverify/src/meta"
+)
+
+func TestEqualsMatching(t *testing.T) {
+	testCases := [][]meta.TypesMap{
+		[]meta.TypesMap{
+			meta.NewEmptyTypesMap(1),
+			meta.NewEmptyTypesMap(1),
+		},
+		[]meta.TypesMap{
+			meta.NewTypesMapFromMap(map[string]struct{}{"string": struct{}{}, "int": struct{}{}}),
+			meta.NewTypesMapFromMap(map[string]struct{}{"string": struct{}{}, "int": struct{}{}}),
+		},
+	}
+
+	for _, testCase := range testCases {
+		if !testCase[0].Equals(testCase[1]) {
+			t.Errorf("%v and %v must match", testCase[0], testCase[1])
+		}
+		if !testCase[1].Equals(testCase[0]) {
+			t.Errorf("%v and %v must match", testCase[1], testCase[0])
+		}
+	}
+}
+
+func TestEqualNonMatching(t *testing.T) {
+	testCases := [][]meta.TypesMap{
+		[]meta.TypesMap{
+			meta.NewEmptyTypesMap(1),
+			meta.NewTypesMapFromMap(map[string]struct{}{"int": struct{}{}}),
+		},
+		[]meta.TypesMap{
+			meta.NewTypesMapFromMap(map[string]struct{}{"string": struct{}{}}),
+			meta.NewTypesMapFromMap(map[string]struct{}{"int": struct{}{}}),
+		},
+	}
+
+	for _, testCase := range testCases {
+		if testCase[0].Equals(testCase[1]) {
+			t.Errorf("%v and %v must not match", testCase[0], testCase[1])
+		}
+		if testCase[1].Equals(testCase[0]) {
+			t.Errorf("%v and %v must not match", testCase[1], testCase[0])
+		}
+	}
+}
