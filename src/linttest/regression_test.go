0722b36e8a44db3c5a18878d73fca8b3880558c8
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-04-12 15:58:53 +0300

    solver: fix trait members accessLevel issues (#398)
    * solver: fix trait members accessLevel issues
    
    Since traits can't extend or implement anything,
    it's 100% guarantee that if we find a symbol inside
    a trait it comes from that exact trait.
    
    We can use that to achieve PHP-like semantics of
    traits access level.
    
    Suppose we're looking for a symbol S in class C that
    uses trait T. Class C doesn't have it so we fall back
    into traits. If trait T has S, return it with
    implClass=C, so it looks like C itself had it.
    
    That way, we can check accessLevel using the unmodified
    canAccess() method.
    
    Fixes #283
    Fixes #209
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>
    
    * solver: make findX functions return result structs
    
    Return both TraitName and ClassName in both
    FindMethod and FindProperty functions.
    
    Result.ImplName() returns the name that refers
    to the implementation type name.
    
    Result.ClassName is a type name that should be
    used when resolving the access levels.
    
    Result.TraitName is set to a trait name if Result.Info is
    derived from the trait. Result.ClassName would be
    a name of type that "uses" a trait.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- src/linttest/regression_test.go
+++ src/linttest/regression_test.go
@@ -1107,3 +1107,126 @@ $_ = $x instanceof Box ? 0 : 1;
 	}
 	test.RunAndMatch()
 }
+
+func TestIssue283(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+trait YummyProps {
+  public $price = 'fair';
+  protected $taste = 'good';
+  private $secret = 'sauce';
+}
+
+class Borsch {
+  use YummyProps;
+
+  /** @return string */
+  public function getTaste() {
+    return $this->taste; // OK: using protected trait prop from embedding class
+  }
+
+  /** @return string */
+  public function getSecret() {
+    return $this->secret; // OK: using private trait prop from embedding class
+  }
+}
+
+class SaltyBorsch extends Borsch {
+  protected function getTaste2() {
+    return $this->taste; // OK: using protected trait prop from inherited class
+  }
+  protected function getSecret2() {
+    return $this->secret; // Bad: used private trait prop from inherited class
+  }
+}
+
+$borsch = new Borsch();
+$_ = $borsch->taste; // Bad: can't access protected trait prop from the outside
+$_ = $borsch->price; // OK: can use public trait prop here
+
+$borsch2 = new SaltyBorsch();
+$_ = $borsch2->price; // OK: can also access public trait prop from derived class
+`)
+	test.Expect = []string{
+		`Cannot access private property \Borsch->secret`,
+		`Cannot access protected property \Borsch->taste`,
+	}
+	test.RunAndMatch()
+}
+
+func TestIssue209_1(t *testing.T) {
+	linttest.SimpleNegativeTest(t, `<?php
+trait A {
+  private function priv() { return 1; }
+  protected function prot() { return 2; }
+  /** @return int */
+  public function pub() { return 3; }
+}
+
+class B {
+  use A;
+  /** @return int */
+  public function sum() {
+    return $this->priv() + $this->prot() + $this->pub();
+  }
+}
+
+echo (new B)->sum(); // actual PHP prints 6
+`)
+}
+
+func TestIssue209_2(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+trait Methods {
+  /***/
+  public function pubMethod() {}
+  protected function protMethod() {}
+  private function privMethod() {}
+
+  /***/
+  public static function staticPubMethod() {}
+  protected static function staticProtMethod() {}
+  private static function staticPrivMethod() {}
+}
+
+class Base {
+  use Methods;
+
+  private function f() {
+    $this->pubMethod();
+    $this->protMethod();
+    $this->privMethod();
+  }
+}
+
+class Derived extends Base {
+  private function f() {
+    $this->pubMethod();
+    $this->protMethod();
+    $this->privMethod(); // Bad: can't call private
+    Base::staticPubMethod();
+    Base::staticProtMethod();
+    parent::staticProtMethod();
+    parent::staticPrivMethod(); // Bad: can't call private
+  }
+}
+
+$b = new Base();
+$b->pubMethod();
+$b->protMethod(); // Bad: can't call from the outside
+$b->privMethod(); // Bad: can't call from the outside
+
+Base::staticPubMethod();
+Derived::staticPubMethod();
+Base::staticProtMethod();
+`)
+	test.Expect = []string{
+		`Cannot access private method \Base->privMethod()`,
+		`Cannot access protected method \Base->protMethod()`,
+		`Cannot access private method \Base->privMethod()`,
+		`Cannot access private method \Base::staticPrivMethod()`,
+		`Cannot access protected method \Base::staticProtMethod()`,
+	}
+	test.RunAndMatch()
+}
