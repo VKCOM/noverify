d9ab69bbcfce036c6af0164dfcca5dc6a1f82581
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-07-03 11:22:59 +0300

    implement baseline linting mode (#530)
    -output-baseline is a new output mode that will produce baseline
    file instead of normal reports. This option is used to generate
    a suppression profile.
    
    -baseline option enables baseline mode and uses a provided
    suppression profile to filter out the output.
    
    -conservative-baseline flag disables hashing of the prev and next
    lines of the source code line being reported. This can be useful
    if we want the behavior that is closer to the original diff mode,
    but it may be more prone to false negatives. We need to collect
    some stats on diagnostics and see which ones of them produce
    more hash collisions on the real projects (can be solved by
    making their report message more detailed and dynamic).
    
    Baseline file is stored in JSON format and has several
    size optimizations in 3 metrics:
    
    	1. Line width
    	2. Line numbers
    	3. Overall file size
    
    (1) and (2) are important to ensure that a file is openable
    in most text editors. It also makes a file more git-friendly.
    
    Warnings are converted to a 64-bit hash using the following fields:
    
    	* Filename (only base)
    	* Previous source line (in non-conservative mode only)
    	* Start source line
    	* Next soure line (in non-conservative mode only)
    	* Warning message (includes a check name)
    	* Warning scope (global/func/method name)
    
    Updates #525
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ src/baseline/baseline_test.go
@@ -0,0 +1,192 @@
+package baseline
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestWriteReadBaseline(t *testing.T) {
+	type testHashFields struct {
+		Lines     [3]string
+		CheckName string
+		Message   string
+		Scope     string
+	}
+
+	makeReports := func(filename string, testFieldsList []testHashFields) map[uint64]Report {
+		fieldsList := make([]HashFields, len(testFieldsList))
+		counters := make(map[uint64]int, len(testFieldsList))
+		for i, fields := range testFieldsList {
+			fieldsList[i] = HashFields{
+				Filename:  filename,
+				PrevLine:  []byte(fields.Lines[0]),
+				StartLine: []byte(fields.Lines[1]),
+				NextLine:  []byte(fields.Lines[2]),
+				CheckName: fields.CheckName,
+				Message:   fields.Message,
+				Scope:     fields.Scope,
+			}
+			fieldsList[i].Filename = filename
+			counters[ReportHash(fieldsList[i])]++
+		}
+
+		reports := make(map[uint64]Report, len(fieldsList))
+		for _, fields := range fieldsList {
+			hash := ReportHash(fields)
+			reports[hash] = Report{
+				Hash:  hash,
+				Count: counters[hash],
+			}
+		}
+
+		return reports
+	}
+	makeFileProfile := func(filename string, testFieldsList []testHashFields) FileProfile {
+		return FileProfile{
+			Filename: filename,
+			Reports:  makeReports(filename, testFieldsList),
+		}
+	}
+	makeProfile := func(files ...FileProfile) *Profile {
+		m := make(map[string]FileProfile, len(files))
+		for _, f := range files {
+			m[f.Filename] = f
+		}
+		return &Profile{Files: m}
+	}
+
+	const expectedOutput = `{
+	"Version": 1,
+	"Stats": {
+		"CountTotal": 0,
+		"CountPerCheck": null
+	},
+	"Files": [
+		{
+			"File": "/a/Bar.php",
+			"Hashes": [
+				"zr6nxkhb4gdh"
+			]
+		},
+		{
+			"File": "/a/Foo.php",
+			"Hashes": [
+				"19nhmzlil1g8o,1f8786sb08uia,35fa1tga0130h*2"
+			]
+		}
+	]
+}
+`
+
+	f1 := makeFileProfile("/a/Foo.php", []testHashFields{
+		// Two same code patterns inside one method.
+		{
+			Lines: [3]string{
+				"  if ($cond) {",
+				"    die('test');",
+				"  }",
+			},
+			Scope:     "Foo::init",
+			CheckName: "noDie",
+			Message:   "Do not debug with die('test')",
+		},
+		{
+			Lines: [3]string{
+				"  if ($cond) {",
+				"    die('test');",
+				"  }",
+			},
+			Scope:     "Foo::init",
+			CheckName: "noDie",
+			Message:   "Do not debug with die('test')",
+		},
+
+		// die('test') with different lines around it.
+		{
+			Lines: [3]string{
+				"  if (false) {",
+				"    die('test');",
+				"  }",
+			},
+			Scope:     "Foo::init",
+			CheckName: "noDie",
+			Message:   "Do not debug with die('test')",
+		},
+
+		// Completely different warning.
+		{
+			Lines: [3]string{
+				"    $i++;",
+				"    $foo->save($i);",
+				"  }",
+			},
+			Scope:     "foo_increment_counter",
+			CheckName: "noDie",
+			Message:   "Calling undefined method $foo->save()",
+		},
+	})
+
+	f2 := makeFileProfile("/a/Bar.php", []testHashFields{
+		// More die('test'), now inside another class.
+		{
+			Lines: [3]string{
+				"  if ($cond) {",
+				"    die('test');",
+				"  }",
+			},
+			Scope:     "Bar::init",
+			CheckName: "noDie",
+			Message:   "Do not debug with die('test')",
+		},
+	})
+
+	x := makeProfile(f1, f2)
+
+	// Run test more than once to verify that the output is stable.
+	for i := 0; i < 10; i++ {
+		var buf bytes.Buffer
+		if err := WriteProfile(&buf, x, &Stats{}); err != nil {
+			t.Fatalf("iter=%d error while encoding profile: %v", i, err)
+		}
+		if diff := cmp.Diff(buf.String(), expectedOutput); diff != "" {
+			t.Fatalf("iter=%d printed output differs:\n%s", i, diff)
+		}
+
+		y, err := ReadProfile(&buf)
+		if err != nil {
+			t.Fatalf("iter=%d error while reading encoded profile: %v", i, err)
+		}
+
+		if diff := cmp.Diff(x, y); diff != "" {
+			t.Fatalf("iter=%d decoded profile differs:\n%s", i, diff)
+		}
+	}
+
+	// Test bigger profile size.
+	// This test is useful to monitor how our changes affect
+	// the profile size.
+	files := make([]FileProfile, 200)
+	for i := range files {
+		fieldsList := make([]testHashFields, 30)
+		for j := range fieldsList {
+			fieldsList[j] = testHashFields{
+				Lines:   [3]string{"a", "b", "c"},
+				Scope:   fmt.Sprintf("func%d", i),
+				Message: "baseline file size test",
+			}
+		}
+		files[i] = makeFileProfile(fmt.Sprintf("file%d.php", i), fieldsList)
+	}
+	bigProfile := makeProfile(files...)
+	var buf bytes.Buffer
+	if err := WriteProfile(&buf, bigProfile, &Stats{}); err != nil {
+		t.Fatalf("encoding big profile: %v", err)
+	}
+	expectedSize := 15535
+	if expectedSize != buf.Len() {
+		t.Fatalf("big profile size differs:\nhave: %d\nwant: %d", buf.Len(), expectedSize)
+	}
+}
