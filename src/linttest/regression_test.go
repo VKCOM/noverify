388737771c1d51f3fcfd8eaf2660307975db499a
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2020-07-04 18:02:16 +0300

    solver: handle interfaces in Implements() (#557)
    Added 2 cases:
    - Implements(I, I) => true
    - Implements(I1 extends I2, I2) => true
    
    We could separate loop in class-only and interface-only paths,
    but it seems we can't tell whether a ClassInfo represents
    a class or an interface.
    
    Fixes #556
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- src/linttest/regression_test.go
+++ src/linttest/regression_test.go
@@ -1384,3 +1384,35 @@ class A {
   }
 }`)
 }
+
+func TestIssue556(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.LoadStubs = []string{`stubs/phpstorm-stubs/Core/Core_c.php`}
+	test.AddFile(`<?php
+/**
+ * @param \ArrayAccess|array $v
+ */
+function f1($v) {
+  return $v[10];
+}
+
+interface MyArrayAccess extends ArrayAccess {}
+
+/**
+ * @param MyArrayAccess|array $v
+ */
+function f2($v) {
+  return $v[10];
+}
+
+interface MyArrayAccess2 extends MyArrayAccess {}
+
+/**
+ * @param MyArrayAccess2 $v
+ */
+function f3($v) {
+  return $v[10];
+}
+`)
+	test.RunAndMatch()
+}
