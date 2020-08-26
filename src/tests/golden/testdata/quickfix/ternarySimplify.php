7c9e2a0e46e3793cae8127292030884b510ab5dd
i582 <i58215@voenmeh.ru>
2020-08-26 17:33:54 +0300

    tests/golden: added test for pure $x ? $x : $y

--- src/tests/golden/testdata/quickfix/ternarySimplify.php
+++ src/tests/golden/testdata/quickfix/ternarySimplify.php
@@ -1,6 +1,15 @@
 <?php
 
+$a = 5;
+function f() {
+    $a++;
+    return $a;
+}
+
 function ternarySimplify() {
     $z = $x ? $x : 5;
+    // f not pure
+    $z = f() ? f() : 5;
+
     $z = isset($x) ? $x : $y;
 }
