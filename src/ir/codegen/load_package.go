3332529f6f3198e9ebf1a0922e611e32144664ad
Iskander Sharipov <quasilyte@gmail.com>
2020-09-01 15:36:00 +0300

    ir: auto-generate most nodes related code + codegen refactoring
    These functions/methods became auto-generated:
    
    	* Walk() method for every node
    	* GetFreeFloating() method for every node
    	* ir.GetPosition() function
    	* ir.GetNodeKind() function
    
    Fixes #739
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ src/ir/codegen/load_package.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/types"
+	"path"
+)
+
+func loadPackage(typechecker *types.Config, ctx *context, dir string) (*packageData, error) {
+	pkgName := path.Base(dir)
+	parsedPkgs, err := parser.ParseDir(ctx.fset, dir, nil, 0)
+	if err != nil {
+		return nil, err
+	}
+	astPkg, ok := parsedPkgs[pkgName]
+	if !ok {
+		return nil, fmt.Errorf("package %s not found", pkgName)
+	}
+	astFiles := make([]*ast.File, 0, len(astPkg.Files))
+	for _, f := range astPkg.Files {
+		astFiles = append(astFiles, f)
+	}
+	var info types.Info
+	typesPkg, err := typechecker.Check(pkgName, ctx.fset, astFiles, &info)
+	if err != nil {
+		return nil, err
+	}
+	root := typesPkg.Scope()
+	result := &packageData{
+		scope: root,
+	}
+	nodeObject := root.Lookup("Node")
+	nodeIface, ok := nodeObject.Type().Underlying().(*types.Interface)
+	if !ok {
+		return nil, fmt.Errorf("can't find ir.Node type")
+	}
+	ctx.nodeIface = nodeIface
+	for _, sym := range root.Names() {
+		tn, ok := root.Lookup(sym).(*types.TypeName)
+		if !ok {
+			continue
+		}
+		named, ok := tn.Type().(*types.Named)
+		if !ok {
+			continue
+		}
+		structType, ok := named.Underlying().(*types.Struct)
+		if !ok {
+			continue
+		}
+		if !types.Implements(types.NewPointer(named), nodeIface) {
+			continue
+		}
+		result.types = append(result.types, &typeData{
+			name: tn.Name(),
+			info: structType,
+		})
+	}
+
+	return result, nil
+}
