c8b1f27e882c41e0e0c14566ba4ee211f6bf3a19
Mikhail Fesenko <proggga@gmail.com>
2020-10-25 02:49:01 +0300

    Split big switch to methods calls, remove dulicate code

--- src/langsrv/defs.go
+++ src/langsrv/defs.go
@@ -44,223 +44,230 @@ func (d *definitionWalker) EnterNode(n ir.Node) bool {
 
 	switch n := n.(type) {
 	case *ir.FunctionCallExpr:
-		pos := ir.GetPosition(n.Function)
+		return d.processFunctionCallExpr(n)
 
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		var fun meta.FuncInfo
-		var ok bool
-		var nameStr string
-
-		if nm, isName := n.Function.(*ir.Name); isName {
-			if nm.IsFullyQualified() {
-				nameStr = nm.Value
-				fun, ok = meta.Info.GetFunction(nameStr)
-			} else {
-				nameStr = nm.Value
-				fun, ok = meta.Info.GetFunction(d.st.Namespace + `\` + nameStr)
-				if !ok && d.st.Namespace != "" {
-					fun, ok = meta.Info.GetFunction(`\` + nameStr)
-				}
-			}
-		}
-
-		if ok {
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(fun.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(fun.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(fun.Pos.Line) - 1},
-				},
-			})
-		}
-
-		lintdebug.Send("Found function %s: %s:%d", nameStr, fun.Pos.Filename, fun.Pos.Line)
 	case *ir.StaticCallExpr:
-		pos := ir.GetPosition(n.Call)
+		return d.processStaticCallExpr(n)
 
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		lintdebug.Send("Static call found")
-
-		// not going to resolve $obj->$someMethod(); calls
-		id, ok := n.Call.(*ir.Identifier)
-		if !ok {
-			lintdebug.Send("Static Call is not identifier")
-			return true
-		}
-
-		className, ok := solver.GetClassName(&d.st, n.Class)
-		if !ok {
-			return true
-		}
-
-		m, ok := solver.FindMethod(className, id.Value)
-		if ok {
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(m.Info.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(m.Info.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(m.Info.Pos.Line) - 1},
-				},
-			})
-		}
 	case *ir.MethodCallExpr:
-		pos := ir.GetPosition(n.Method)
+		return d.processMethodCallExpr(n)
 
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		lintdebug.Send("Method call found")
-
-		if len(d.foundScopes) == 0 {
-			lintdebug.Send("No scope found")
-			return true
-		}
-
-		foundScope := d.foundScopes[len(d.foundScopes)-1]
-
-		// not going to resolve $obj->$someMethod(); calls
-		id, ok := n.Method.(*ir.Identifier)
-		if !ok {
-			lintdebug.Send("Method is not identifier")
-			return true
-		}
-
-		types := safeExprType(foundScope, &d.st, n.Variable)
-
-		types.Iterate(func(t string) {
-			m, ok := solver.FindMethod(t, id.Value)
-			if !ok {
-				lintdebug.Send("Could not find method for %s::%s", t, id.Value)
-				return
-			}
-
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(m.Info.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(m.Info.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(m.Info.Pos.Line) - 1},
-				},
-			})
-		})
 	case *ir.PropertyFetchExpr:
-		pos := ir.GetPosition(n.Property)
+		return d.processPropertyFetchExpr(n)
 
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		lintdebug.Send("Property found")
-
-		if len(d.foundScopes) == 0 {
-			lintdebug.Send("No scope found")
-			return true
-		}
-
-		foundScope := d.foundScopes[len(d.foundScopes)-1]
-
-		// not going to resolve $obj->$someProperty
-		id, ok := n.Property.(*ir.Identifier)
-		if !ok {
-			lintdebug.Send("Method is not identifier")
-			return true
-		}
-
-		types := safeExprType(foundScope, &d.st, n.Variable)
-
-		types.Iterate(func(t string) {
-			p, ok := solver.FindProperty(t, id.Value)
-			if !ok {
-				lintdebug.Send("Could not find property for %s->%s", t, id.Value)
-				return
-			}
-
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(p.Info.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(p.Info.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(p.Info.Pos.Line) - 1},
-				},
-			})
-		})
 	case *ir.ConstFetchExpr:
-		pos := ir.GetPosition(n.Constant)
+		return d.processConstFetchExpr(n)
 
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		_, c, ok := solver.GetConstant(&d.st, n.Constant)
-
-		if ok {
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(c.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(c.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(c.Pos.Line) - 1},
-				},
-			})
-		}
 	case *ir.ClassConstFetchExpr:
-		if pos := ir.GetPosition(n.ConstantName); d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		constName := n.ConstantName
-
-		if constName.Value == `class` || constName.Value == `CLASS` {
-			return false
-		}
-
-		className, ok := solver.GetClassName(&d.st, n.Class)
-		if !ok {
-			return false
-		}
-
-		if c, _, ok := solver.FindConstant(className, constName.Value); ok {
-			d.result = append(d.result, vscode.Location{
-				URI: uri.File(c.Pos.Filename),
-				Range: vscode.Range{
-					Start: vscode.Position{Line: int(c.Pos.Line) - 1},
-					End:   vscode.Position{Line: int(c.Pos.Line) - 1},
-				},
-			})
-		}
+		return d.processClassConstFetchExpr(n)
 
 	case *ir.Name:
-		pos := ir.GetPosition(n)
-
-		if d.position > pos.EndPos || d.position < pos.StartPos {
-			return true
-		}
-
-		className, ok := solver.GetClassName(&d.st, n)
-		if !ok {
-			return true
-		}
-
-		c, ok := meta.Info.GetClassOrTrait(className)
-
-		if !ok {
-			return true
-		}
-
-		d.result = append(d.result, vscode.Location{
-			URI: uri.File(c.Pos.Filename),
-			Range: vscode.Range{
-				Start: vscode.Position{Line: int(c.Pos.Line) - 1},
-				End:   vscode.Position{Line: int(c.Pos.Line) - 1},
-			},
-		})
+		return d.processName(n)
 	}
 
 	return true
+}
+
+func (d *definitionWalker) processName(n *ir.Name) bool {
+	pos := ir.GetPosition(n)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	className, ok := solver.GetClassName(&d.st, n)
+	if !ok {
+		return true
+	}
+
+	c, ok := meta.Info.GetClassOrTrait(className)
+
+	if !ok {
+		return true
+	}
+
+	d.appendResult(c.Pos.Filename, int(c.Pos.Line)-1, int(c.Pos.Line)-1)
+	return true
+}
+
+func (d *definitionWalker) processClassConstFetchExpr(n *ir.ClassConstFetchExpr) bool {
+	if pos := ir.GetPosition(n.ConstantName); d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	constName := n.ConstantName
+
+	if constName.Value == `class` || constName.Value == `CLASS` {
+		return false
+	}
+
+	className, ok := solver.GetClassName(&d.st, n.Class)
+	if !ok {
+		return false
+	}
+
+	if c, _, ok := solver.FindConstant(className, constName.Value); ok {
+		d.appendResult(c.Pos.Filename, int(c.Pos.Line)-1, int(c.Pos.Line)-1)
+	}
+	return true
+}
+
+func (d *definitionWalker) processConstFetchExpr(n *ir.ConstFetchExpr) bool {
+	pos := ir.GetPosition(n.Constant)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	_, c, ok := solver.GetConstant(&d.st, n.Constant)
+
+	if ok {
+		d.appendResult(c.Pos.Filename, int(c.Pos.Line)-1, int(c.Pos.Line)-1)
+	}
+	return true
+}
+
+func (d *definitionWalker) processPropertyFetchExpr(n *ir.PropertyFetchExpr) bool {
+	pos := ir.GetPosition(n.Property)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	lintdebug.Send("Property found")
+
+	if len(d.foundScopes) == 0 {
+		lintdebug.Send("No scope found")
+		return true
+	}
+
+	foundScope := d.foundScopes[len(d.foundScopes)-1]
+
+	// not going to resolve $obj->$someProperty
+	id, ok := n.Property.(*ir.Identifier)
+	if !ok {
+		lintdebug.Send("Method is not identifier")
+		return true
+	}
+
+	types := safeExprType(foundScope, &d.st, n.Variable)
+
+	types.Iterate(func(t string) {
+		p, ok := solver.FindProperty(t, id.Value)
+		if !ok {
+			lintdebug.Send("Could not find property for %s->%s", t, id.Value)
+			return
+		}
+
+		d.appendResult(p.Info.Pos.Filename, int(p.Info.Pos.Line)-1, int(p.Info.Pos.Line)-1)
+	})
+	return true
+}
+
+func (d *definitionWalker) processMethodCallExpr(n *ir.MethodCallExpr) bool {
+	pos := ir.GetPosition(n.Method)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	lintdebug.Send("Method call found")
+
+	if len(d.foundScopes) == 0 {
+		lintdebug.Send("No scope found")
+		return true
+	}
+
+	foundScope := d.foundScopes[len(d.foundScopes)-1]
+
+	// not going to resolve $obj->$someMethod(); calls
+	id, ok := n.Method.(*ir.Identifier)
+	if !ok {
+		lintdebug.Send("Method is not identifier")
+		return true
+	}
+
+	types := safeExprType(foundScope, &d.st, n.Variable)
+
+	types.Iterate(func(t string) {
+		p, ok := solver.FindMethod(t, id.Value)
+		if !ok {
+			lintdebug.Send("Could not find method for %s::%s", t, id.Value)
+			return
+		}
+
+		d.appendResult(p.Info.Pos.Filename, int(p.Info.Pos.Line)-1, int(p.Info.Pos.Line)-1)
+	})
+	return true
+}
+
+func (d *definitionWalker) processFunctionCallExpr(n *ir.FunctionCallExpr) bool {
+	pos := ir.GetPosition(n.Function)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	var fun meta.FuncInfo
+	var ok bool
+	var nameStr string
+
+	if nm, isName := n.Function.(*ir.Name); isName {
+		nameStr = nm.Value
+		if nm.IsFullyQualified() {
+			fun, ok = meta.Info.GetFunction(nameStr)
+		} else {
+			fun, ok = meta.Info.GetFunction(d.st.Namespace + `\` + nameStr)
+			if !ok && d.st.Namespace != "" {
+				fun, ok = meta.Info.GetFunction(`\` + nameStr)
+			}
+		}
+	}
+
+	if ok {
+		d.appendResult(fun.Pos.Filename, int(fun.Pos.Line)-1, int(fun.Pos.Line)-1)
+	}
+
+	lintdebug.Send("Found function %s: %s:%d", nameStr, fun.Pos.Filename, fun.Pos.Line)
+	return true
+}
+
+func (d *definitionWalker) processStaticCallExpr(n *ir.StaticCallExpr) bool {
+	pos := ir.GetPosition(n.Call)
+
+	if d.position > pos.EndPos || d.position < pos.StartPos {
+		return true
+	}
+
+	lintdebug.Send("Static call found")
+
+	// not going to resolve $obj->$someMethod(); calls
+	id, ok := n.Call.(*ir.Identifier)
+	if !ok {
+		lintdebug.Send("Static Call is not identifier")
+		return true
+	}
+
+	className, ok := solver.GetClassName(&d.st, n.Class)
+	if !ok {
+		return true
+	}
+
+	p, ok := solver.FindMethod(className, id.Value)
+	if ok {
+		d.appendResult(p.Info.Pos.Filename, int(p.Info.Pos.Line)-1, int(p.Info.Pos.Line)-1)
+	}
+	return true
+}
+
+func (d *definitionWalker) appendResult(file string, start, end int) {
+	d.result = append(d.result, vscode.Location{
+		URI: uri.File(file),
+		Range: vscode.Range{
+			Start: vscode.Position{Line: start},
+			End:   vscode.Position{Line: end},
+		},
+	})
 }
 
 // LeaveNode is invoked after node process
