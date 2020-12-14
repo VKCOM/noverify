9cca9921354670f7ea9bcd96f703a269814673ba
i582 <i58215@voenmeh.ru>
2020-12-14 16:54:21 +0300

    main: added message "No critical issues found. Your code is perfect." for non-diff mode

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -249,6 +249,7 @@ func mainNoExit(ruleSets []*rules.Set, args *cmdlineArguments, cfg *MainConfig) 
 		log.Printf("Found %d critical reports", criticalReports)
 		return 2, nil
 	}
+	log.Printf("No critical issues found. Your code is perfect.")
 	return 0, nil
 }
 
