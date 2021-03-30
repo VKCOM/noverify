f5f59c9b96823ae17453c61c5e70afa4a23d9b49
i582 <i58215@voenmeh.ru>
2021-03-30 12:49:41 +0300

    linter: fix golangci-lint issue

--- src/linter/root_context.go
+++ src/linter/root_context.go
@@ -1,7 +1,6 @@
 package linter
 
 import (
-	"fmt"
 	"path/filepath"
 
 	"github.com/VKCOM/noverify/src/baseline"
@@ -40,14 +39,6 @@ func newRootContext(config *Config, workerCtx *WorkerContext, st *meta.ClassPars
 		baseline:       p,
 		shapes:         map[string]autogen.ShapeTypeInfo{},
 	}
-}
-
-func (ctx *rootContext) generateShapeName() string {
-	// We'll probably generate names for anonymous classes in the
-	// same way in future. All auto-generated names should end with "$".
-	// `\shape$` prefix makes it easy to check whether a type
-	// is a shape without looking it up inside classes map.
-	return fmt.Sprintf(`\shape$%s$%d$`, ctx.st.CurrentFile, len(ctx.shapes))
 }
 
 func newTypesMap(ctx *rootContext, types []meta.Type) meta.TypesMap {
