598751ec5c635113bf2ae1f145aeca293e376c9b
i582 <i58215@voenmeh.ru>
2021-04-05 20:02:52 +0300

    tests/checkers: added some test

--- src/tests/checkers/disable_inspection_test.go
+++ src/tests/checkers/disable_inspection_test.go
@@ -10,38 +10,63 @@ func TestDisableDeadCode(t *testing.T) {
 	linttest.SimpleNegativeTest(t, `<?php
 function f() {
 #ifndef KPHP
-    return 1;
+  return 1;
 #endif
 
-    /** @noinspection PhpUnreachableStatementInspection */
-    return 10;
+  /** @noinspection PhpUnreachableStatementInspection */
+  return 10;
 }
 
 function f1() {
 #ifndef KPHP
-    return 1;
+  return 1;
 #endif
 
-    /** @noinspection PhpUnreachableStatementInspection */
-	$a = 100;
+  /** @noinspection PhpUnreachableStatementInspection */
+  $a = 100;
 
-    return $a;
+  return $a;
 }
 
 function f2() {
 #ifndef KPHP
-    return 1;
+  return 1;
 #endif
 
 #ifndef KPHP2
-	/** @noinspection PhpUnreachableStatementInspection */
-    return 1;
+  /** @noinspection PhpUnreachableStatementInspection */
+  return 1;
 #endif
 
-    /** @noinspection PhpUnreachableStatementInspection */
-	$a = 100;
+  /** @noinspection PhpUnreachableStatementInspection */
+  $a = 100;
 
-    return $a;
+  return $a;
+}
+
+function f3() {
+#ifndef KPHP
+  return 1;
+#endif
+
+  /* @noinspection PhpUnreachableStatementInspection */
+  $a = 100;
+
+  return $a;
+}
+
+function f4() {
+#ifndef KPHP
+  return 1;
+#endif
+
+  /**
+   * @noinspection PhpUnreachableStatementInspection 
+   * Some comment
+   */
+  $a = 100;
+
+  return $a;
 }
 `)
 }
