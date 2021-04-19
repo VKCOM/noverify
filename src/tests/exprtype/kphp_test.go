6b7eb968b8540760894fbd64b1ca3cd28c920c80
Makhnev Petr <51853996+i582@users.noreply.github.com>
2021-04-19 11:34:59 +0300

    meta: add type inferring for array_filter_by_key and instance_cache_f… (#975)

--- src/tests/exprtype/kphp_test.go
+++ src/tests/exprtype/kphp_test.go
@@ -55,6 +55,39 @@ function f() {
 	runKPHPExprTypeTest(t, &exprTypeTestParams{code: code, stubs: "<?php /* no code */"})
 }
 
+func TestInstanceCacheFetch(t *testing.T) {
+	code := `<?php
+class Foo {
+	/** Method */
+	public function method() {}
+}
+
+const CLASS_NAME = "Foo";
+
+class Boo {
+	/** Method */
+	public function method() {
+		$text = "";
+		exprtype(instance_cache_fetch(self::class, $text), "\Boo|null");
+		exprtype(instance_cache_fetch(static::class, $text), "\Boo|null");
+		exprtype(instance_cache_fetch($this::class, $text), "\Boo|null");
+	}
+}
+
+function f() {
+	$text = "";
+	$className = "";
+
+	exprtype(instance_cache_fetch(Foo::class, $text), "\Foo|null");
+	exprtype(instance_cache_fetch("Foo", $text), "\Foo|null");
+	exprtype(instance_cache_fetch(10, $text), "mixed");
+	exprtype(instance_cache_fetch($className, $text), "mixed");
+	exprtype(instance_cache_fetch(CLASS_NAME, $text), "mixed");
+}
+`
+	runKPHPExprTypeTest(t, &exprTypeTestParams{code: code, stubs: "<?php /* no code */"})
+}
+
 func TestArrayFirstLastType(t *testing.T) {
 	code := `<?php
 class Foo {}
@@ -136,6 +169,61 @@ function f1() {
 
 	$m = array_first_value();
 	exprtype($m, "mixed");
+}
+`
+	runKPHPExprTypeTest(t, &exprTypeTestParams{code: code, stubs: "<?php /* no code */"})
+}
+
+func TestArrayFilterByKey(t *testing.T) {
+	code := `<?php
+class Foo {}
+
+/**
+ * @return Foo[]
+ */
+function returnFooArray() {
+	return [new Foo, new Foo, new Foo];
+}
+
+/**
+ * @return mixed
+ */
+function returnMixed() {}
+
+/**
+ * @return mixed[]
+ */
+function returnMixedArray() {}
+
+function f() {
+	$a = [10, 20, 30];
+	$b = array_filter_by_key($a, function() { return true; });
+	exprtype($b, "int[]");
+	
+	$c = [new Foo, new Foo, new Foo];
+	$d = array_filter_by_key($c, function() { return true; });
+	exprtype($d, "\Foo[]");
+
+	$e = returnFooArray();
+	$f = array_filter_by_key($e, function() { return true; });
+	exprtype($f, "\Foo[]");
+
+	$g = returnMixed();
+	$h = array_filter_by_key($g, function() { return true; });
+	exprtype($h, "mixed");
+
+	$i = returnMixedArray();
+	$j = array_filter_by_key($i, function() { return true; });
+	exprtype($j, "mixed[]");
+
+	$k = array_filter_by_key([10, 20], function() { return true; });
+	exprtype($k, "int[]");
+
+	$l = array_filter_by_key([10, "hello"], function() { return true; });
+	exprtype($l, "mixed[]");
+
+	$m = array_filter_by_key([], function() { return true; });
+	exprtype($m, "mixed[]");
 }
 `
 	runKPHPExprTypeTest(t, &exprTypeTestParams{code: code, stubs: "<?php /* no code */"})
