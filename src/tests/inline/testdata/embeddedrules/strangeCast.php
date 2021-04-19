f022eff917da5f12494d6f130334bdf4c1d2a366
i582 <i58215@voenmeh.ru>
2021-04-19 12:19:40 +0300

    tests/inline/embeddedrules: fix

--- src/tests/inline/testdata/embeddedrules/strangeCast.php
+++ src/tests/inline/testdata/embeddedrules/strangeCast.php
@@ -3,14 +3,16 @@
 function strangeCast() {
     $x = 100;
 
-    $_ = $x.""; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
-    $_ = $x.''; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
+    $_ = $x . ""; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
+    $_ = "" . $x; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
+    $_ = $x . ''; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
+    $_ = '' . $x; // want `concatenation with empty string, possible type cast, use explicit cast to string instead of concatenate with empty string`
 
     $y = "10";
 
-    $_ = $y + 0; // want `addition with zero, possible type cast, use an explicit cast to int or float instead of zero addition`
+    $_ = 0 + $y; // want `addition with zero, possible type cast, use an explicit cast to int or float instead of zero addition`
     $_ = $y + 10; // ok
-    $_ = $y + 0.0; // want `addition with zero, possible type cast, use an explicit cast to int or float instead of zero addition`
+    $_ = 0.0 + $y; // want `addition with zero, possible type cast, use an explicit cast to int or float instead of zero addition`
 
     $string = "10";
 
