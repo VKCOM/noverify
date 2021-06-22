62881c0b2514db4648fc93f1db8f04b792db38a7
Makhnev Petr <51853996+i582@users.noreply.github.com>
2021-06-22 12:20:42 +0300

    linttest: improved inline tests (#998)
    Now each file is used only to check the checker
    with the name of the file name equal, all other
    errors will be ignored.
    
    If the file has a name for which there is no
    checker, an error will be thrown.
    
    Fixed an error message if the want directive
    is written for the line, but there is no actual
    error for this line.

--- src/tests/inline/testdata/checkers/typeHint.php
+++ src/tests/inline/testdata/checkers/typeHint.php
@@ -5,12 +5,10 @@ class Boo {}
 class FooBad {
     public parent $a; // want `cannot use 'parent' typehint when current class has no parent`
 
-    /** */
     public function f1(): parent { // want `cannot use 'parent' typehint when current class has no parent`
         return new Boo;
     }
 
-    /** */
     public function f2(parent $a) { // want `cannot use 'parent' typehint when current class has no parent`
         return new Boo;
     }
@@ -19,12 +17,10 @@ class FooBad {
 class FooOk extends Boo {
     public parent $a;
 
-    /** */
     public function f(): parent {
         return new Boo;
     }
 
-    /** */
     public function f2(parent $a) {
         return new Boo;
     }
