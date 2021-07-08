d8b00cc23607abb1d52ce999e7868f4a2a662307
i582 <i58215@voenmeh.ru>
2021-07-08 14:21:07 +0300

    linter: improved test

--- src/tests/checkers/basic_test.go
+++ src/tests/checkers/basic_test.go
@@ -1859,10 +1859,20 @@ func TestSwitchBreak(t *testing.T) {
 func TestNameCase(t *testing.T) {
 	test := linttest.NewSuite(t)
 	test.AddFile(`<?php
-class FooBar {
-  public function method_a() {}
+class Bar {
+  const TheConst2 = 10;
+}
 
-   const TheConst = 10;
+class FooBar extends Bar {
+  const TheConst = 10;
+
+  public function method_a() {
+    echo self::TheConst;
+    echo static::TheConst;
+    echo parent::TheConst2;
+
+    $_ = FOObar::TheConst;
+  }
 }
 
 class Baz extends foobar {}
@@ -1881,6 +1891,7 @@ $_ = FOObar::TheConst;
 		`\foobar should be spelled \FooBar`,
 		`Method_a should be spelled method_a`,
 		`\func_A should be spelled \func_a`,
+		`\FOObar should be spelled \FooBar`,
 		`\FOObar should be spelled \FooBar`,
 	}
 	linttest.RunFilterMatch(test, `nameMismatch`)
