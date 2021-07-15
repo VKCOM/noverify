5046127e43b9204fcc7748e2cbea2efa8bc09dd0
i582 <i58215@voenmeh.ru>
2021-07-16 01:06:07 +0300

    phpdoctypes: added a check that the return type is defined for the callable(...) annotation

---
+++ src/tests/checkers/closure_test.go
@@ -0,0 +1,70 @@
+package checkers
+
+import (
+	"testing"
+
+	"github.com/VKCOM/noverify/src/linttest"
+)
+
+func TestClosureCapture(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+class omg {
+  public $some_property;
+}
+
+function doSomething($a, omg $b) {
+  return function() use($b) {
+    echo $b->some_property;
+    echo $b->other_property;
+  };
+}`,
+	)
+	test.Expect = []string{
+		"other_property does not exist",
+	}
+	test.RunAndMatch()
+}
+
+func TestClosureDoc(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+class Foo {
+  /**
+   * @return int
+   */
+  public function method(): int { return 0; }
+}
+
+/**
+ * @param callable(int, string): Boo|Foo $s
+ */
+function f(callable $s) {
+  $a = $s(10);
+  echo $a->method();
+}
+`,
+	)
+	test.Expect = []string{
+		"Too few arguments for anonymous(int,string): Boo|Foo defined in PHPDoc, expecting 2, saw 1",
+	}
+	test.RunAndMatch()
+}
+
+func TestClosureInvalidDoc(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+class Foo {}
+
+/**
+ * @param callable(int, string) $s
+ * @param callable(int, string): Foo $s1
+ */
+function f(callable $s, callable $s1) {}
+`,
+	)
+	test.Expect = []string{
+		"Lost return type for callable(...), if the function returns nothing, specify void explicitly",
+	}
+	test.RunAndMatch()
+}
