218ba66d0bd7b6f16a0303e829d4c770e15348b4
i582 <i58215@voenmeh.ru>
2021-07-16 07:09:26 +0300

    types: fixed issues

--- src/types/compatible.go
+++ src/types/compatible.go
@@ -12,44 +12,33 @@ type ClassData struct {
 }
 
 type Compatible struct {
-	GetClassByType func(name string) (ClassData, bool)
+	classDataProvider func(name string) (ClassData, bool)
 }
 
-// CompatibleTypes T1 is Wanted, T2 is Gotten
-func (c *Compatible) CompatibleTypes(T1, T2 Map) (ok bool, desc string) {
-	return compatibleTypes(T1, T2, c)
+func (c *Compatible) CompatibleTypes(t1, t2 Map) (ok bool, desc string) {
+	return compatibleTypes(t1, t2, c)
 }
 
-func (c *Compatible) CompatibleType(T1, T2 string) (ok bool, desc string) {
-	return compatibleType(T1, T2, c)
+func (c *Compatible) CompatibleType(t1, t2 string) (ok bool, desc string) {
+	return compatibleType(t1, t2, c)
 }
 
-func (c *Compatible) getClassByType(name string) (*ClassData, bool) {
-	class, ok := c.GetClassByType(name)
-	if !ok {
-		return nil, false
-	}
-
-	return &class, true
-}
-
-// CompatibleTypes T1 is Wanted, T2 is Gotten
-func compatibleTypes(T1, T2 Map, c *Compatible) (ok bool, desc string) {
-	if T1.Empty() || T2.Empty() {
+func compatibleTypes(t1, t2 Map, c *Compatible) (ok bool, desc string) {
+	if t1.Empty() || t2.Empty() {
 		return true, ""
 	}
 
-	ok, desc = compatibleOneWithMany(T1, T2, c)
+	ok, desc = compatibleOneWithMany(t1, t2, c)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleOneWithMany(T2, T1, c)
+	ok, desc = compatibleOneWithMany(t2, t1, c)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleManyWithMany(T1, T2, c)
+	ok, desc = compatibleManyWithMany(t1, t2, c)
 	if !ok {
 		return false, desc
 	}
@@ -57,22 +46,22 @@ func compatibleTypes(T1, T2 Map, c *Compatible) (ok bool, desc string) {
 	return true, ""
 }
 
-func compatibleManyWithMany(T1 Map, T2 Map, c *Compatible) (ok bool, desc string) {
-	if T1.Len() == 1 || T2.Len() == 1 {
+func compatibleManyWithMany(t1 Map, t2 Map, c *Compatible) (ok bool, desc string) {
+	if t1.Len() == 1 || t2.Len() == 1 {
 		return true, ""
 	}
 
-	if T1.Contains("null") && !T2.Contains("null") {
-		return false, fmt.Sprintf("cannot use nullable %s as %s", T1, T2)
+	if t1.Contains("null") && !t2.Contains("null") {
+		return false, fmt.Sprintf("cannot use nullable %s as %s", t1, t2)
 	}
-	if !T1.Contains("null") && T2.Contains("null") {
-		return false, fmt.Sprintf("cannot use %s as nullable %s", T1, T2)
+	if !t1.Contains("null") && t2.Contains("null") {
+		return false, fmt.Sprintf("cannot use %s as nullable %s", t1, t2)
 	}
 
 	var compatibleWithOne bool
 
-	T1.Iterate(func(T1Typ string) {
-		T2.Iterate(func(T2Typ string) {
+	t1.Iterate(func(T1Typ string) {
+		t2.Iterate(func(T2Typ string) {
 			ok, _ = compatibleType(T1Typ, T2Typ, c)
 			if ok {
 				compatibleWithOne = true
@@ -83,28 +72,28 @@ func compatibleManyWithMany(T1 Map, T2 Map, c *Compatible) (ok bool, desc string
 	return compatibleWithOne, ""
 }
 
-func compatibleOneWithMany(T1 Map, T2 Map, c *Compatible) (ok bool, desc string) {
-	if T1.Len() == 1 {
-		if T2.Len() == 1 {
-			return compatibleType(T1.String(), T2.String(), c)
+func compatibleOneWithMany(t1 Map, t2 Map, c *Compatible) (ok bool, desc string) {
+	if t1.Len() == 1 {
+		if t2.Len() == 1 {
+			return compatibleType(t1.String(), t2.String(), c)
 		}
 
-		T1S := T1.String()
+		T1S := t1.String()
 
 		if strings.Contains(T1S, "mixed") {
 			return true, ""
 		}
 
-		T2IsNullable := T2.Find(func(typ string) bool {
+		T2IsNullable := t2.Find(func(typ string) bool {
 			return typ == "null"
 		})
 		if T2IsNullable {
-			return false, fmt.Sprintf("cannot use type %s as nullable type %s", T1S, T2.String())
+			return false, fmt.Sprintf("cannot use type %s as nullable type %s", T1S, t2.String())
 		}
 
 		var compatibleWithOne bool
 
-		T2.Iterate(func(typ string) {
+		t2.Iterate(func(typ string) {
 			ok, desc = compatibleType(T1S, typ, c)
 			if ok {
 				compatibleWithOne = true
@@ -112,57 +101,57 @@ func compatibleOneWithMany(T1 Map, T2 Map, c *Compatible) (ok bool, desc string)
 		})
 
 		if !compatibleWithOne {
-			return false, fmt.Sprintf("none of the possible types (%s) are compatible with %s", T2.String(), T1S)
+			return false, fmt.Sprintf("none of the possible types (%s) are compatible with %s", t2.String(), T1S)
 		}
 	}
 
 	return true, ""
 }
 
-func CompatibleType(T1, T2 string) (ok bool, desc string) {
-	return compatibleType(T1, T2, nil)
+func CompatibleType(t1, t2 string) (ok bool, desc string) {
+	return compatibleType(t1, t2, nil)
 }
 
-func compatibleType(T1, T2 string, c *Compatible) (ok bool, desc string) {
-	if T1 == "mixed" || T2 == "mixed" {
+func compatibleType(t1, t2 string, c *Compatible) (ok bool, desc string) {
+	if t1 == "mixed" || t2 == "mixed" {
 		return true, ""
 	}
 
-	if IsPOD(T1) && IsPOD(T2) {
-		if compatibleBoolean(T1, T2) {
+	if IsPOD(t1) && IsPOD(t2) {
+		if compatibleBoolean(t1, t2) {
 			return true, ""
 		}
 
-		if T1 != T2 {
-			return false, fmt.Sprintf("cannot use %s as %s", T1, T2)
+		if t1 != t2 {
+			return false, fmt.Sprintf("cannot use %s as %s", t1, t2)
 		}
 	}
 
-	T1A, ok := Alias(T1)
+	T1A, ok := Alias(t1)
 	if ok {
-		T1 = T1A
+		t1 = T1A
 	}
-	T2A, ok := Alias(T1)
+	T2A, ok := Alias(t1)
 	if ok {
-		T2 = T2A
+		t2 = T2A
 	}
 
-	ok, desc = compatibleClass(T1, T2, c)
+	ok, desc = compatibleClass(t1, t2, c)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleArray(T1, T2, c)
+	ok, desc = compatibleArray(t1, t2, c)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleCallable(T1, T2)
+	ok, desc = compatibleCallable(t1, t2)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleCallable(T2, T1)
+	ok, desc = compatibleCallable(t2, t1)
 	if !ok {
 		return false, desc
 	}
@@ -170,95 +159,99 @@ func compatibleType(T1, T2 string, c *Compatible) (ok bool, desc string) {
 	return true, ""
 }
 
-func (c *Compatible) compatibleClassWithInheritance(T1, T2 ClassData) (ok bool, desc string) {
-	if T1.Name == T2.Name {
+func (c *Compatible) compatibleClassWithInheritance(t1, t2 ClassData) (ok bool, desc string) {
+	if t1.Name == t2.Name {
 		return true, ""
 	}
 
-	if T1.Parent != "" {
-		T1Parent, ok := c.GetClassByType(T1.Parent)
+	if t1.Parent != "" {
+		T1Parent, ok := c.classDataProvider(t1.Parent)
 		if ok {
-			ok, desc = c.compatibleClassWithInheritance(T1Parent, T2)
+			ok, desc = c.compatibleClassWithInheritance(T1Parent, t2)
 			if ok {
 				return true, ""
+			} else {
+				return false, desc
 			}
 		}
 	}
 
-	if len(T1.Interfaces) > 0 {
-		for iface := range T1.Interfaces {
-			T1Iface, ok := c.GetClassByType(iface)
+	if len(t1.Interfaces) > 0 {
+		for iface := range t1.Interfaces {
+			T1Iface, ok := c.classDataProvider(iface)
 			if ok {
-				ok, desc = c.compatibleClassWithInheritance(T1Iface, T2)
+				ok, desc = c.compatibleClassWithInheritance(T1Iface, t2)
 				if ok {
 					return true, ""
+				} else {
+					return false, desc
 				}
 			}
 		}
 	}
 
-	return false, fmt.Sprintf("cannot use class %s as class %s", T1.Name, T2.Name)
+	return false, fmt.Sprintf("cannot use class %s as class %s", t1.Name, t2.Name)
 }
 
-func compatibleClass(T1 string, T2 string, c *Compatible) (ok bool, desc string) {
-	namesEquals := func(T1, T2 string) (ok bool, desc string) {
-		if T1 != T2 {
-			return false, fmt.Sprintf("cannot use class %s as class %s", T1, T2)
+func compatibleClass(t1 string, t2 string, c *Compatible) (ok bool, desc string) {
+	namesEquals := func(t1, t2 string) (ok bool, desc string) {
+		if t1 != t2 {
+			return false, fmt.Sprintf("cannot use class %s as class %s", t1, t2)
 		}
 
 		return true, ""
 	}
 
-	if IsClass(T1) && IsClass(T2) {
+	if IsClass(t1) && IsClass(t2) {
 		if c == nil {
-			return namesEquals(T1, T2)
+			return namesEquals(t1, t2)
 		}
 
-		T1Class, ok := c.GetClassByType(T1)
+		T1Class, ok := c.classDataProvider(t1)
 		if !ok {
-			return namesEquals(T1, T2)
+			return namesEquals(t1, t2)
 		}
-		T2Class, ok := c.GetClassByType(T2)
+		T2Class, ok := c.classDataProvider(t2)
 		if !ok {
-			return namesEquals(T1, T2)
+			return namesEquals(t1, t2)
 		}
 
-		ok, desc = c.compatibleClassWithInheritance(T1Class, T2Class)
+		ok, _ = c.compatibleClassWithInheritance(T1Class, T2Class)
 		if ok {
 			return true, ""
 		}
 
-		ok, desc = c.compatibleClassWithInheritance(T2Class, T1Class)
+		ok, _ = c.compatibleClassWithInheritance(T2Class, T1Class)
 		if ok {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use class %s as class %s", T1, T2)
+		return false, fmt.Sprintf("cannot use class %s as class %s", t1, t2)
 	}
 
-	if IsClass(T1) {
-		if T2 == "object" {
+	if IsClass(t1) {
+		if t2 == "object" {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use class %s as %s", T1, T2)
+		return false, fmt.Sprintf("cannot use class %s as %s", t1, t2)
 	}
 
-	if IsClass(T2) {
-		if T1 == "object" {
+	if IsClass(t2) {
+		if t1 == "object" {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use %s as class %s", T1, T2)
+		return false, fmt.Sprintf("cannot use %s as class %s", t1, t2)
 	}
 
 	return true, ""
 }
 
-func compatibleArray(T1 string, T2 string, c *Compatible) (ok bool, desc string) {
-	if IsArray(T1) && IsArray(T2) {
-		T1El := ArrayElementType(T1)
-		T2El := ArrayElementType(T2)
+func compatibleArray(t1 string, t2 string, c *Compatible) (ok bool, desc string) {
+	if IsArray(t1) && IsArray(t2) {
+		T1El := ArrayElementType(t1)
+		T2El := ArrayElementType(t2)
 
 		comp, des := compatibleType(T1El, T2El, c)
 		if !comp {
@@ -268,28 +261,28 @@ func compatibleArray(T1 string, T2 string, c *Compatible) (ok bool, desc string)
 		return true, ""
 	}
 
-	if IsArray(T1) {
-		if T2 == "iterable" {
+	if IsArray(t1) {
+		if t2 == "iterable" {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use array of %s as %s", T1, T2)
+		return false, fmt.Sprintf("cannot use array of %s as %s", t1, t2)
 	}
 
-	if IsArray(T2) {
-		if T1 == "iterable" {
+	if IsArray(t2) {
+		if t1 == "iterable" {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use %s as array of %s", T1, T2)
+		return false, fmt.Sprintf("cannot use %s as array of %s", t1, t2)
 	}
 
-	ok, desc = compatibleIterable(T1, T2)
+	ok, desc = compatibleIterable(t1, t2)
 	if !ok {
 		return false, desc
 	}
 
-	ok, desc = compatibleIterable(T2, T1)
+	ok, desc = compatibleIterable(t2, t1)
 	if !ok {
 		return false, desc
 	}
@@ -297,44 +290,44 @@ func compatibleArray(T1 string, T2 string, c *Compatible) (ok bool, desc string)
 	return true, ""
 }
 
-func compatibleIterable(T1 string, T2 string) (ok bool, desc string) {
-	if T1 == "iterable" {
-		if T2 == "iterable" {
+func compatibleIterable(t1 string, t2 string) (ok bool, desc string) {
+	if t1 == "iterable" {
+		if t2 == "iterable" {
 			return true, ""
 		}
 
-		if IsClass(T2) {
+		if IsClass(t2) {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use %s as %s", T1, T2)
+		return false, fmt.Sprintf("cannot use %s as %s", t1, t2)
 	}
 
 	return true, ""
 }
 
-func compatibleCallable(T1, T2 string) (ok bool, desc string) {
-	if T1 == "callable" {
-		if T2 == "string" {
+func compatibleCallable(t1, t2 string) (ok bool, desc string) {
+	if t1 == "callable" {
+		if t2 == "string" {
 			return true, ""
 		}
 
-		if T2 == "callable" || IsClosure(T2) || IsClass(T2) {
+		if t2 == "callable" || IsClosure(t2) || IsClass(t2) {
 			return true, ""
 		}
 
-		return false, fmt.Sprintf("cannot use %s as %s", T1, T2)
+		return false, fmt.Sprintf("cannot use %s as %s", t1, t2)
 	}
 
 	return true, ""
 }
 
-func compatibleBoolean(T1, T2 string) bool {
-	if T1 == "bool" {
-		return T2 == "bool" || T2 == "true" || T2 == "false"
+func compatibleBoolean(t1, t2 string) bool {
+	if t1 == "bool" {
+		return t2 == "bool" || t2 == "true" || t2 == "false"
 	}
-	if T2 == "bool" {
-		return T1 == "bool" || T1 == "true" || T1 == "false"
+	if t2 == "bool" {
+		return t1 == "bool" || t1 == "true" || t1 == "false"
 	}
 	return false
 }
