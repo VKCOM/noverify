a51cd3ac44b83300fd803afaa9df95439f0904b7
i582 <51853996+i582@users.noreply.github.com>
2021-10-12 17:48:57 +0300

    all: removed old tests

--- test_rules/tests/simpleRule.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function simpleRuleTest(): int {
-  $a = 100;
-  $a++; // want `Some error message`
-  return $a;
-}
--- test_rules/tests/simpleRule22_any.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function simpleRuleTest(): int {
-  $a = 100;
-  $a--;
-  return $a;
-}
