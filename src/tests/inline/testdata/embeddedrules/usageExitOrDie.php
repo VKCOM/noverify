d677950a14c121e1d1628fded798427c8910d371
Danil Ovchinnikov <danil42worker@gmail.com>
2022-05-02 23:09:52 +0700

    embeddedrules: fix code style

--- src/tests/inline/testdata/embeddedrules/usageExitOrDie.php
+++ src/tests/inline/testdata/embeddedrules/usageExitOrDie.php
@@ -1,9 +1,9 @@
 <?php
 
 function usageExitOrDie() {
-    exit; // want `Don't use the 'exit' function`
-    exit(1); // want `Don't use the 'exit' function`
+  exit; // want `Don't use the 'exit' function`
+  exit(1); // want `Don't use the 'exit' function`
 
-    die; // want `Don't use the 'die' function`
-    die("die"); // want `Don't use the 'die' function`
+  die; // want `Don't use the 'die' function`
+  die("die"); // want `Don't use the 'die' function`
 }
