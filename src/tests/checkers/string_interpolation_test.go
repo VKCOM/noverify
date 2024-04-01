987d5f09ce48b3d26586326debf36ef9ad5d1a98
Richardas Kuchinskas <r.kuchinskas@vk.team>
2024-04-01 17:33:33 +0300

    updated solution

--- src/tests/checkers/string_interpolation_test.go
+++ src/tests/checkers/string_interpolation_test.go
@@ -1,8 +1,9 @@
 package checkers
 
 import (
-	"github.com/VKCOM/noverify/src/linttest"
 	"testing"
+
+	"github.com/VKCOM/noverify/src/linttest"
 )
 
 func TestInterpolationDeprecated1(t *testing.T) {
