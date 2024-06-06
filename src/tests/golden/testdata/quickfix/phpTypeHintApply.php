ac2b32a92b068cd3aa90ff71b3e3ff354946bf90
Richardas Kuchinskas <r.kuchinskas@vk.team>
2024-06-06 19:41:51 +0300

    new tests for typeHints

---
+++ src/tests/golden/testdata/quickfix/phpTypeHintApply.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+class SimpleClass
+{
+
+/**
+ * This is what the variable does. The var line contains the type stored in this variable.
+ * @var string
+ */
+private string $foo;
+
+
+/**
+ * This is what the variable does. The var line contains the type stored in this variable.
+ * @var string
+ */
+private string $foo2 ;
+
+/**
+ * @param $str string
+ * @param $str2 string
+ */
+function test($str, $str2){
+}
+}
+
+/**
+ * @param $str string
+ */
+function test2($str){
+}
