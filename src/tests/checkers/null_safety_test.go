88b0bb0c5f7babf78bd66b5bf40f74d0bd29c971
Richardas Kuchinskas <r.kuchinskas@vk.team>
2025-02-17 14:52:03 +0300

    new tests

--- src/tests/checkers/null_safety_test.go
+++ src/tests/checkers/null_safety_test.go
@@ -155,3 +155,89 @@ testVariadic(new A(), null);
 	}
 	test.RunAndMatch()
 }
+
+// TODO: After realisation Control Flow Graph (CFG) и Data Flow Graph (DFG) this test must fail
+func TestIfNullCheckSafe(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+declare(strict_types=1);
+
+class A {
+    public string $value = 'Safe';
+}
+
+/**
+ * Function expecting a non-null instance of A.
+ */
+function test(A $a): void {
+    echo $a->value;
+}
+
+$v = null;
+if ($v == null) {
+    // Correctly assign a new instance when $v is null.
+    $v = new A();
+}
+test($v); // Should be safe.
+`)
+
+	test.Expect = []string{
+		`not null safety call in function test signature of param`,
+	}
+	test.RunAndMatch()
+}
+
+func TestIfNullCheckUnsafe(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+declare(strict_types=1);
+
+class A {
+    public string $value = 'Unsafe';
+}
+
+/**
+ * Function expecting a non-null instance of A.
+ */
+function test(A $a): void {
+    echo $a->value;
+}
+
+$v = null;
+if ($v == null) {
+    // No assignment is done here – $v remains null.
+}
+test($v); // Should trigger a null safety error.
+`)
+	test.Expect = []string{
+		"not null safety call in function test signature of param",
+	}
+	test.RunAndMatch()
+}
+
+func TestIfNotNullCheck(t *testing.T) {
+	test := linttest.NewSuite(t)
+	test.AddFile(`<?php
+declare(strict_types=1);
+
+class A {
+    public string $value = 'NotNull';
+}
+
+/**
+ * Function expecting a non-null instance of A.
+ */
+function test(A $a): void {
+    echo $a->value;
+}
+
+$v = new A();
+if ($v !== null) {
+    // $v is known to be non-null.
+    test($v); // Should be safe.
+}
+`)
+
+	test.Expect = []string{}
+	test.RunAndMatch()
+}
